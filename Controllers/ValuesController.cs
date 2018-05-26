34bb776df342908d52bcc6c813be7f9a2b7abe28
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-25 21:54:23 -0300

    Renomeando projeto Webapi

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace radix.gateway.Controllers
+namespace Radix.Gateway.WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace radix.gateway
+namespace Radix.Gateway.WebApi
 {
     public class Startup
     {
