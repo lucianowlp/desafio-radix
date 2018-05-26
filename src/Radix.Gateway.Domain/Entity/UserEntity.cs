2e037b958852a70c0e97b969aa508f75c9f4c8b9
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-26 00:07:53 -0300

    Adicionando middleware básico para utilizando do webapi. Configurando a utlização do formato json e adicionando configuração de cors para permitir requisições de qualquer origem. Inicializando entidade de usuário.

---
+++ src/Radix.Gateway.Domain/Entity/UserEntity.cs
@@ -0,0 +1,11 @@
+namespace Radix.Gateway.Domain
+{
+    public class UserEntity
+    {
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Endereco { get; set; }
+        public bool SistemasAntifraudes { get; set; }
+        public string adquirente { get; set; }
+    }
+}
