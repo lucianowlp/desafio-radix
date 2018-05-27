78c41531594eafcc5ec24ae1efc4bb13087b6d7a
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-27 11:24:24 -0300

    criação dos contratos para transação com cartão de crédito.

---
+++ src/Radix.Gateway.Client/DataContracts/CreditCard/CreditCardTransaction.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Radix.Gateway.Client.DataContracts.CreditCard
+{
+    [DataContract(Namespace = "")]
+    public class CreditCardTransaction : BaseResponse
+    {
+        [DataMember]
+        public Payment Payment { get; set; }
+    }
+}
---
+++ src/Radix.Gateway.Client/EnumTypes/PaymentStatusType.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Radix.Gateway.Client.EnumTypes
+{
+    /// <summary>
+    /// Status dos pagamentos
+    /// </summary>
+    [DataContract]
+    public enum PaymentStatusType
+    {
+        /// <summary>
+        /// Autorizado
+        /// </summary>
+        [EnumMember]
+        Authorized = 1,
+
+        /// <susmmary>
+        /// Não autorizado
+        /// </summary>
+        [EnumMember]
+        NotAuthorized = 2,
+    }
+}
