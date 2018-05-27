a911d97e6d10105ae31a17764a7a76df60712a09
Luciano Mattos <desenv.mattos@hotmail.com>
2018-05-27 10:56:00 -0300

    Adicionando variaveis de ambientes das integrações com as api da stone e da cielo. Criação do contrato com os dados do cartão de crédito.

---
+++ src/Radix.Gateway.Client/DataContracts/CreditCard/CreditCard.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.Serialization;
+
+namespace Radix.Gateway.Client.DataContracts.CreditCard
+{
+    [DataContract(Namespace = "")]
+    public class CreditCard
+    {
+        /// <summary>
+        /// Número do cartão de crédito
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string CreditCardNumber { get; set; }
+
+        /// <summary>
+        /// Nome impresso no cartão
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string PrintedName { get; set; }
+
+        /// <summary>
+        /// Mês de expiração do cartão de crédito
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int ExpMonth { get; set; }
+
+        /// <summary>
+        /// Ano de expiração do cartão de crédito
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int ExpYear { get; set; }
+
+        /// <summary>
+        /// Código de segurança - CVV
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string SecurityCode { get; set; }
+
+
+    }
+}
